232e3cdeb58e45c8eb7ce5636120095e7993d46f
Minh <minhtranconglis@gmail.com>
2022-12-13 05:57:16 +0700

    dev

--- prisma_api/src/controllers/user.controllers.ts
+++ prisma_api/src/controllers/user.controllers.ts
@@ -1,22 +1,30 @@
 import { Prisma, PrismaClient } from "@prisma/client"
+import { hashSync, genSaltSync } from 'bcrypt'
+
 const prisma = new PrismaClient()
 import e, { Request, Response } from "express"
 export const createUser = async (req: Request, res: Response) => {
     try {
         console.log(req.body);
-        const createUser = await prisma.user.create({ data: req.body })
+        if (req.body.password) {
+            const salt = genSaltSync(Number(process.env.SALT_HASH));
+            const hash_password: string = hashSync(req.body.password, salt);
+            const createUser = await prisma.user.create({
+                data: req.body
+            })
+        }
         res.status(200).json(createUser)
     } catch (error) {
         res.status(500).json({ error });
     }
 }
 
-export const emailResigter = async(req: Request, res: Response ) => {
+export const emailResigter = async (req: Request, res: Response) => {
     let email = req.body.email;
-    const result = await prisma.user.findFirst({where: {email: email}})
+    const result = await prisma.user.findFirst({ where: { email: email } })
     if (!result) {
-		res.json({ isInUse: false })
-	} else {
-		res.json({ isInUse: true })
-	}
+        res.json({ isInUse: false })
+    } else {
+        res.json({ isInUse: true })
+    }
 }
