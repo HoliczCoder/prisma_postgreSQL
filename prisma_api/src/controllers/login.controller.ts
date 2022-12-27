cf3128f1b911731324c8ff89d6cc649e736c418e
MinhNoob <minhtranconglis@gmail.com>
2022-12-27 17:04:18 +0700

    dev

--- prisma_api/src/controllers/login.controller.ts
+++ prisma_api/src/controllers/login.controller.ts
@@ -7,14 +7,43 @@ const prisma = new PrismaClient();
 
 export const login = async (req: Request, res: Response) => {
   try {
-    let { user, password } = req.body;
+    // let { user, password } = req.body;
     let email = req.body.email;
+    let password = req.body.password
+    
     const result = await prisma.user.findFirst({ where: { email: email } });
     if (!result) {
-      res.status(500).send({ error: "Email  no encontrado" });
+      res.status(500).send({ error: "Email not exist" });
       return;
     } else {
-      res.json({ isInUse: true });
+      // res.json({ result: result });
+      if (!compareSync(password, result.password)) {
+        res.status(403).json({ msg: "incorrect password" });
+      } else {
+        let setDate = new Date();
+        setDate.setHours(setDate.getHours() + 4);
+        // const usuario: object = {
+        //   idUsuario: usuarioConectado.idUsuario,
+        //   institucion: usuarioConectado.institucion,
+        //   nombreUsuario: usuarioConectado.nombreUsuario,
+        //   descripcion: usuarioConectado.descripcion,
+        //   email: usuarioConectado.email,
+        //   rol: usuarioConectado.rol,
+        // };
+        const user: object = {
+          userName: result.name
+        }
+        const crendential = {
+          expireDate: `setDate`,
+          user: {
+            userName: result.name
+          },
+          token: jwt.sign({ user }, "testing", {
+            expiresIn: process.env.EXPIRACION_TOKEN,
+          }),
+        };
+        res.json(crendential);
+      }
     }
   } catch (e) {
     console.log(e);
--- prisma_api/src/routers/routes.index.ts
+++ prisma_api/src/routers/routes.index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import userRouter from "./users.routes";
+import loginRouter from "./login.routes"
 import { Request, Response } from "express";
 const routes = Router();
 // routes.use("/", (req: Request, res: Response) => {
 //   res.send("hell world");
 // });
+
 routes.use("/user", userRouter);
+routes.use("/login", loginRouter )
 export default routes;
