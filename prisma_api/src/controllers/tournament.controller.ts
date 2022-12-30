de410f49460a554ab9f41ba9deb05b0aae30ed5b
MinhNoob <minhtranconglis@gmail.com>
2022-12-30 17:34:29 +0700

    dev

---
+++ prisma_api/src/controllers/tournament.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+
+export const createTournament = async (req: Request, res: Response) => {
+    try {
+        // const result = await prisma.tournament.create()
+    } catch (error) {
+        
+    }
+
+}
