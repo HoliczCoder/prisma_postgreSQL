ef9feea7b2f2ec8a3e8334313c0370a639f476f3
MinhNoob <minhtranconglis@gmail.com>
2023-01-09 09:38:10 +0700

    dev

--- prisma_api/src/controllers/problem.controller.ts
+++ prisma_api/src/controllers/problem.controller.ts
@@ -2,48 +2,70 @@ import { Request, Response } from "express";
 import * as jwt from "jsonwebtoken";
 import { compareSync } from "bcrypt";
 import { Prisma, PrismaClient } from "@prisma/client";
+import { json } from "body-parser";
 const prisma = new PrismaClient();
 
 export const createProblem = async (req: Request, res: Response) => {
-    console.log("hello world");
-
-    try {
-        const createProblem = await prisma.problem.create({
-            data: {
-                name: req.body.name,
-                statement: req.body.statement,
-                complexityId: req.body.complexityId,
-                maxExecutionTime: req.body.maxExecutionTime,
-                problemHashtab: {
-                    create: [
-                        {
-                            hashtag: {
-                                create: {
-                                    hashtag: req.body.hashtag,
-                                },
-                            },
-                        },
-                    ],
+  try {
+    if (req?.body?.inputParameter) {
+      const createProblem = await prisma.problem.create({
+        data: {
+          name: req.body.name,
+          statement: req.body.statement,
+          complexityId: req.body.complexityId,
+          maxExecutionTime: req.body.maxExecutionTime,
+          problemHashtab: {
+            create: [
+              {
+                hashtag: {
+                  create: {
+                    hashtag: req.body.hashtag,
+                  },
                 },
-                solution: {
-                    create: [
-                        {
-                            inputParameter: req.body.inputParameter,
-                            exit: req.body.exit,
-                        }
-                    ]
-                }
-            },
-        })
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({ e, msg: "Error" });
-
+              },
+            ],
+          },
+          solution: {
+            create: [
+              {
+                inputParameter: req.body.inputParameter,
+                exit: req.body.exit,
+              },
+            ],
+          },
+        },
+      });
+      res.json(createProblem);
+    } else {
+      const createProblem = await prisma.problem.create({
+        data: {
+          name: req.body.name,
+          statement: req.body.statement,
+          complexityId: req.body.complexityId,
+          maxExecutionTime: req.body.maxExecutionTime,
+          problemHashtab: {
+            create: [
+              {
+                hashtag: {
+                  create: {
+                    hashtag: req.body.hashtag,
+                  },
+                },
+              },
+            ],
+          },
+        },
+      });
+      res.json(createProblem);
     }
-}
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ e, msg: "Error" });
+  }
+};
 
 export const getProblem = async (req: Request, res: Response) => {
-    res.json({
-        "hello": "this is response"
-    })
-}
+  res.json({
+    hello: "this is response",
+  });
+};
